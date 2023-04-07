a1df9b4ef28a363259f0092b5f4fb8a866bb2103
yuliangyong <yuliangyong8295019@gmail.com>
2023-04-07 10:05:20 +0200

    remove a todo label in DecimalColumnReader.cpp

--- pixels-core/lib/reader/DecimalColumnReader.cpp
+++ pixels-core/lib/reader/DecimalColumnReader.cpp
@@ -22,7 +22,6 @@ void DecimalColumnReader::read(std::shared_ptr<ByteBuffer> input, pixels::proto:
                                pixels::proto::ColumnChunkIndex chunkIndex) {
     std::shared_ptr<DecimalColumnVector> columnVector =
             std::static_pointer_cast<DecimalColumnVector>(vector);
-    // TODO: type precision and type scale should be the same as the column vector
 	if(type->getPrecision() != columnVector->getPrecision() || type->getScale() != columnVector->getScale()) {
 		throw InvalidArgumentException("reader of decimal(" + std::to_string(type->getPrecision())
 		                               + "," + std::to_string(type->getScale()) + ") doesn't match the column "
